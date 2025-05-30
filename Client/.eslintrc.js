c47248c705e2b78c778de87cd6e127efafdefe94
Hussain Saify <itssaify.17@live.com>
2025-05-30 20:31:56 +0530

    Initial commit

---
+++ Client/.eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: ['react-app'],
+  rules: {
+    // Add any specific rules here
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  root: true // This ensures ESLint only looks at this config file
+}; 
